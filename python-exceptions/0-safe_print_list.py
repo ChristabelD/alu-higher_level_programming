b56b091fef701a56619a8c330c263b09d15eb14c
root <c.adams@alustudent.com>
2022-07-20 11:57:31 -0700

    kiki

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -1,14 +1,11 @@
 #!/usr/bin/python3
-# A function that prints x elements of a list
-
-
 def safe_print_list(my_list=[], x=0):
-    ret = 0
+    Nmbrep = 0
     for i in range(x):
         try:
             print("{}".format(my_list[i]), end="")
-            ret += 1
+            Nmbrep += 1
         except IndexError:
             break
-        print("")
-        return (ret)
+    print("")
+    return 
