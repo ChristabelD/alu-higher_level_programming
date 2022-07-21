4dc1f7262caee830b46ca32aca2c144b320659ec
root <c.adams@alustudent.com>
2022-07-21 14:47:08 -0700

    boss

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -1,8 +1,14 @@
 #!/usr/bin/python3
-# A function that prints x elements of a list
 
 
 def safe_print_list(my_list=[], x=0):
+    """Print x elememts of a list.
+    Args:
+        my_list (list): The list to print elements from.
+        x (int): The number of elements of my_list to print.
+    Returns:
+        The number of elements printed.
+    """
     ret = 0
     for i in range(x):
         try:
