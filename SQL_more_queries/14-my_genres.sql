93edb6d2a9c6733acbf706ca2e39ada0ce3b3c40
root <c.adams@alustudent.com>
2022-08-05 08:29:09 -0700

    14

--- SQL_more_queries/14-my_genres.sql
+++ SQL_more_queries/14-my_genres.sql
@@ -1,12 +1,7 @@
--- Lists all genres of the show Dexter in the database hbtn_0d_tvshows.
--- Records are ordered by descending rating.
 
-SELECT `name`, SUM(`rate`) AS `rating`
-  FROM `tv_genres` AS g
-       INNER JOIN `tv_show_genres` AS s
-       ON s.`genre_id` = g.`id`
-
-       INNER JOIN `tv_show_ratings` AS r
-       ON r.`show_id` = s.`show_id`
- GROUP BY `name`
- ORDER BY `rating` DESC;
+-- 14-my_genres.sql
+SELECT tv_genres.name FROM tv_genres
+JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
+JOIN tv_shows ON tv_shows.id = tv_show_genres.show_id
+WHERE tv_shows.title = 'Dexter'
+ORDER BY tv_genres.name ASC;
