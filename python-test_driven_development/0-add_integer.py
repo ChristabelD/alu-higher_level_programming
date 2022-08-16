59ced9cabfe010a445b590491e86a102d320facd
root <c.adams@alustudent.com>
2022-08-16 15:35:46 -0700

    0-add_integer.py

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,22 +1,11 @@
 #!/usr/bin/python3
-"""Adds two numbers"""
+"""addition function module"""
 
 
 def add_integer(a, b=98):
-    """
-    Returns sum of a and b
-    - Args :
-        a: int or float
-        b: int or float, default 98
-    """
-    if type(a) == float or type(b) == float:
-        a = int(a)
-        b = int(b)
-
-    if type(a) != int:
-        raise TypeError("a must be an integer")
-    if type(b) != int:
-        raise TypeError("b must be an integer")
-
-    return a + b
-
+    """add two number together"""
+    if type(a) != int and type(a) != float:
+        raise TypeError('a must be an integer')
+    elif type(b) != int and type(b) != float:
+        raise TypeError('b must be an integer')
+    return int(a) + int(b)
