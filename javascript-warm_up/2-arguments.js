ae05b726c6aca91f3f96af10ba53d56839776a33
ChristabelD <C.adams@alustudent.com>
2022-10-10 05:05:38 -0700

    hey

--- javascript-warm_up/2-arguments.js
+++ javascript-warm_up/2-arguments.js
@@ -1,9 +1,12 @@
 #!/usr/bin/node
-const args = process.argv;
-if (args.length === 1) {
-  console.log('Argument found');
-} else if (args.length > 0) {
-  console.log('No argument');
-} else {
-  console.log('Argument found');
+switch (process.argv.length) {
+  case 2:
+    console.log('No argument');
+    break;
+  case 3:
+    console.log('Argument found');
+    break;
+  default:
+    console.log('Arguments found');
+    break;
 }
